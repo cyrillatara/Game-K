5ddb87e723affc22e123f84c10d3e93301b8abe8
Gideon270502 <gideond385@gmail.com>
2021-10-29 14:09:38 +0700

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TournamentComponent } from './tournament/tournament.component';
+
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "profile", component: ProfileComponent },
+  { path: "tournament", component: TournamentComponent },
+  { path: "leaderboard", component: LeaderboardComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent {
+
+  username = "gideonganteng_25";
+  name = "Gideon";
+  jk = "Laki-laki";
+  kota = "Pontianak"
+
+}
